bd930c85c19486b3ff3839d6c1b7c8aac130bd24
LucasParente95 <137080675+LucasParente95@users.noreply.github.com>
2024-09-03 16:22:31 -0300

    Update and rename arq2 to popup.js

---
+++ src/js/popup.js
@@ -0,0 +1,13 @@
+document.getElementById('start-recording').addEventListener('click', () => {
+    chrome.runtime.sendMessage({ command: 'start-recording' });
+    document.getElementById('status').textContent = 'Gravando...';
+    document.getElementById('start-recording').disabled = true;
+    document.getElementById('stop-recording').disabled = false;
+});
+
+document.getElementById('stop-recording').addEventListener('click', () => {
+    chrome.runtime.sendMessage({ command: 'stop-recording' });
+    document.getElementById('status').textContent = 'Parado. Processando resumo...';
+    document.getElementById('start-recording').disabled = false;
+    document.getElementById('stop-recording').disabled = true;
+});
