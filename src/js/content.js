b66ce046a67f2df432957047726ae681a531e744
LucasParente95 <137080675+LucasParente95@users.noreply.github.com>
2024-09-18 17:56:10 -0300

    Delete src/js/content.js

--- src/js/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.command === 'highlight-text') {
-        const selectedText = window.getSelection().toString();
-        alert(`Texto destacado: ${selectedText}`);
-    }
-});
